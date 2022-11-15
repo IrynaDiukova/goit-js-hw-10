3ac2b28f64026adffe62d8d4d028793ce71bf403
Iryna Hridnova <diukovairyna@ukr.net>
2022-11-15 15:23:24 +0200

    add homeWork

--- src/index.js
+++ src/index.js
@@ -23,13 +23,13 @@ function onInput(event) {
     }
 
     fetchCountries(countryName)
-      .then(notifications)
+      .then(markUp)
       .catch(() => 
         {Notiflix.Notify.failure('Oops, there is no country with that name')}
       );
     }
 
-    function notifications(data) {
+    function markUp(data) {
       if (data.length > 10)
         {Notiflix.Notify.warning('Too many matches found. Please enter a more specific name.');
       return;
@@ -39,20 +39,19 @@ function onInput(event) {
         refs.info.innerHTML = '';
         refs.list.innerHTML = data
             .map(item => {
-                return `<li><img src=${item.flags.svg}width = 40px,height = 30></img>
-                        <span>${item.name.official}</span></li>`;
-            })
+               return `<li><img src=${item.flags.svg} width = 45px, height = 30px></img><span>${item.name.official}</span></li>`;
+      })
             .join('');
         return;
     }  
 
-    refs.list.innerHTML = '';
+  refs.list.innerHTML = '';
     const country = data[0];
-    refs.info.innerHTML = `<div class = 'wrapper'><img src=${country.flags.svg}
-                width = 40px, height = 30></img>
-        <span class = 'name'>${country.name.official}</span>/<div>
-        <p>Capital: <span class = 'capital'>${country.capital}</span></p>
-        <p>Population: <span class = 'population'>${country.population}</span></p>
-        <p>Languages: <span class = 'languages'>${Object.values(country.languages)}</span></p>`;
+    refs.info.innerHTML = `<div class = 'wrapper'><img src=${country.flags.svg} 
+      width = 55px, height = 35px></img><span class = 'name'>${country.name.official
+    }</span></div>
+    <p>Capital: <span class = 'capital'>${country.capital}</span></p>
+    <p>Population: <span class = 'population'>${country.population}</span></p>
+    <p>Languages: <span class = 'languages'>${Object.values(country.languages
+    )}</span></p>`;
 };
-
